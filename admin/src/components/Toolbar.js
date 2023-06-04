d1bc5a6feb621f860eb49453348cf816a7c88b70
Javier Diaz <coderdiaz@gmail.com>
2023-06-03 21:42:43 -0600

    feat: added toolbar component

---
+++ admin/src/components/Toolbar.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Button, Stack, Tooltip } from '@strapi/design-system'
+import styled from 'styled-components'
+import Bold from './Icons/Bold'
+import ClearFormat from './Icons/ClearFormat'
+import { execCommand } from '../utils/commands'
+
+const Toolbar = ({ className }) => {
+  return (
+    <Stack spacing={2} horizontal className={className}>
+      <Tooltip description="Bold">
+        <ToolbarAction variant="tertiary" onClick={() => execCommand('bold')}>
+          <Bold size={16} />
+        </ToolbarAction>
+      </Tooltip>
+      <Tooltip description="Clear format">
+        <ToolbarAction variant="tertiary">
+          <ClearFormat size={16} />
+        </ToolbarAction>
+      </Tooltip>
+    </Stack>
+  )
+}
+
+const ToolbarAction = styled(Button)`
+  width: 1rem;
+  height: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export default Toolbar
