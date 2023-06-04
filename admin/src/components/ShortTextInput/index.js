fcc108c4c569d792defd4c98dd10136a8ec12cc1
Javier Diaz <coderdiaz@gmail.com>
2023-06-03 21:42:18 -0600

    feat: added new fields components

--- admin/src/components/ShortTextInput/index.js
+++ admin/src/components/ShortTextInput/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from 'react'
+import {
+  Field,
+  FieldError,
+  FieldHint,
+  FieldLabel,
+  Flex,
+} from '@strapi/design-system'
+import Input from './Input'
 
-const ShortTextInput = () => {
+const ShortTextInput = (props) => {
+  const {
+    name,
+    labelAction,
+    required,
+    intlLabel,
+    error,
+    description,
+    onChange,
+    value,
+  } = props
+
   return (
-    <div />
-  );
+    <Field
+      name={name}
+      id={name}
+      error={error}
+      hint={description && description.id}
+    >
+      <Flex direction="column" alignItems="flex-start" gap={1}>
+        <FieldLabel action={labelAction} required={required}>
+          {intlLabel.id}
+        </FieldLabel>
+        <Input name={name} onChange={onChange} value={value} />
+        <FieldHint />
+        <FieldError />
+      </Flex>
+    </Field>
+  )
 }
 
-export default ShortTextInput;
+export default ShortTextInput
